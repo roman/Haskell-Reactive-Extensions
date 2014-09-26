b5e10f7c6283f1e124954ac72bf4b191cec45b75
Roman Gonzalez <romanandreg@gmail.com>
2014-09-25 22:01:41 -0700

    Add benchmarks and test-suite to the project

---
+++ rx-core/test/Rx/Observable/TimeoutTest.hs
@@ -0,0 +1,37 @@
+module Rx.Observable.TimeoutTest where
+
+import Tiempo
+import Test.HUnit
+import Test.Hspec
+
+import qualified Rx.Observable as Rx
+
+tests :: Spec
+tests =
+  describe "Rx.Observable.Timeout" $ do
+
+    describe "timeout" $
+      it "fails after a period of time" $ do
+        let source = Rx.foldLeft (+) 0
+                       $ Rx.timeout (microSeconds 0)
+                       $ Rx.doAction print
+                       $ Rx.fromList Rx.newThread ([1..] :: [Int])
+        mResult <- Rx.toMaybe source
+        case mResult of
+          Nothing -> assertBool "" True
+          Just _  ->
+            assertFailure "Expected Observable to fail after period but didn't"
+
+    describe "timeoutWith" $
+      it "doesn't fail when 'completeOnTimeout' is used" $ do
+        let source = Rx.foldLeft (+) 0
+                       $ Rx.timeoutWith (Rx.completeOnTimeout .
+                                         Rx.timeoutDelay (microSeconds 0))
+                       $ Rx.doAction print
+                       $ Rx.fromList Rx.newThread ([1..] :: [Int])
+        mResult <- Rx.toMaybe source
+        case mResult of
+          Nothing ->
+            assertFailure "Expected Observable to complete after period but didn't"
+          Just _  ->
+            assertBool "" True
