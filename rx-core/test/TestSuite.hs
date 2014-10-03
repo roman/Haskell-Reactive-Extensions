ec480e917980681873679694420602b056382cb6
Roman Gonzalez <romanandreg@gmail.com>
2014-10-03 08:58:37 -0700

    Add Merge tests to TestSuite

--- rx-core/test/TestSuite.hs
+++ rx-core/test/TestSuite.hs
@@ -3,9 +3,11 @@ module Main where
 import Test.Hspec (hspec)
 
 import qualified Rx.Observable.FoldTest
+import qualified Rx.Observable.MergeTest
 import qualified Rx.Observable.TimeoutTest
 
 main :: IO ()
 main = hspec $ do
   Rx.Observable.FoldTest.tests
+  Rx.Observable.MergeTest.tests
   Rx.Observable.TimeoutTest.tests
