ff025e9907e1063e998245052fcaa5db963efc2d
Roman Gonzalez <romanandreg@gmail.com>
2014-10-06 17:53:06 -0700

    Improve Rx.Observable.merge test

--- rx-core/test/Rx/Observable/MergeTest.hs
+++ rx-core/test/Rx/Observable/MergeTest.hs
@@ -3,9 +3,8 @@ module Rx.Observable.MergeTest (tests) where
 import Test.HUnit
 import Test.Hspec
 
-import Control.Concurrent (threadDelay, yield)
 import Control.Concurrent.Async (async, wait)
-import Control.Monad (replicateM_)
+import Control.Monad (forM_, replicateM, replicateM_)
 
 import qualified Rx.Observable as Rx
 import qualified Rx.Subject    as Rx
@@ -15,9 +14,12 @@ tests =
   describe "Rx.Observable.Merge" $
     describe "merge" $
       it "completes after all inner Observables are completed" $ do
+        let innerSubjectCount = 10
+        subjects@(firstSubject:subjects1) <-
+          replicateM innerSubjectCount Rx.newPublishSubject
+
         sourceSubject  <- Rx.newPublishSubject
-        innerSubject0  <- Rx.newPublishSubject
-        innerSubject1  <- Rx.newPublishSubject
+
         let source = Rx.foldLeft (+) 0
                        $ Rx.merge
                        $ Rx.toAsyncObservable sourceSubject
@@ -25,24 +27,24 @@ tests =
 
         aResult <- async $ Rx.toMaybe source
 
-        Rx.onNext sourceSubject $ Rx.toAsyncObservable innerSubject0
-        replicateM_ 100  $ Rx.onNext innerSubject0 (1 :: Int)
-        Rx.onNext sourceSubject $ Rx.toAsyncObservable innerSubject1
+
+        forM_  subjects $ \subject -> do
+          Rx.onNext sourceSubject $ Rx.toAsyncObservable subject
+          replicateM_ 100 $ Rx.onNext subject (1 :: Int)
+
         Rx.onCompleted sourceSubject
 
         -- If merge doesn't wait for inner observables
         -- this numbers should not be in the total count
-        replicateM_ 50   $ Rx.onNext innerSubject1 (1 :: Int)
-        replicateM_ 1000 $ Rx.onNext innerSubject0 1
-
-        Rx.onCompleted innerSubject1
-        Rx.onCompleted innerSubject0
+        mapM_ Rx.onCompleted subjects1
+        replicateM_ 50 $ Rx.onNext firstSubject 1
+        mapM_ Rx.onCompleted subjects
 
         mResult <- wait aResult
         case mResult of
           Just result ->
             assertEqual "should be the same as folding"
-                        1150
+                        (innerSubjectCount * 100 + 50)
                         result
           Nothing ->
             assertFailure "Rx failed when it shouldn't have"
