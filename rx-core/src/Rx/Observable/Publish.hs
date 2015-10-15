b8d4b60d3dd2735594fd345b06049bd0cfd56bce
Roman Gonzalez <romanandreg@gmail.com>
2015-10-14 22:50:17 -0700

    Make Rx.Observable.Publish.connect return a Disposable
    This cleanups resources of original subscription

--- rx-core/src/Rx/Observable/Publish.hs
+++ rx-core/src/Rx/Observable/Publish.hs
@@ -1,23 +1,24 @@
 module Rx.Observable.Publish where
 
 
-import Rx.Disposable (dispose, newSingleAssignmentDisposable, setDisposable)
+import Rx.Disposable (Disposable, dispose, newDisposable, newSingleAssignmentDisposable, setDisposable)
 import Rx.Subject (newPublishSubject)
-import Rx.Scheduler (Sync)
 import Rx.Observable.Types
 
-publish :: Observable Sync a -> IO (ConnectableObservable a)
+publish :: Observable s a -> IO (ConnectableObservable a)
 publish source = do
     sourceDisposable <- newSingleAssignmentDisposable
     subject <- newPublishSubject
     let observable = toAsyncObservable subject
     return (ConnectableObservable (subscribeObserver observable)
                                   (connect_ subject sourceDisposable)
-                                  (dispose sourceDisposable))
+                                  (dispose sourceDisposable >> onCompleted subject))
   where
     connect_ subject sourceDisposable = do
       disposable <- subscribeObserver source subject
       setDisposable sourceDisposable disposable
 
-connect :: ConnectableObservable a -> IO ()
-connect = _coConnect
+connect :: ConnectableObservable a -> IO Disposable
+connect obs = do
+  _coConnect obs
+  newDisposable "ConnectableObservable" (_coDisconnect obs)
